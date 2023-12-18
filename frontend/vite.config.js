ffa116e692a86e7fec690ebf1d3983300e92f3b3
Srinivas <1rn19cs154.srinivas@gmail.com>
2023-12-18 14:48:59 +0530

    Added env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server : {
-    proxy : {
-      '/api' : "http://localhost:3000"
+export default defineConfig(({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()],
+    server : {
+      proxy : {
+        '/api' : process.env.VITE_PATH
+      }
     }
   }
 })
